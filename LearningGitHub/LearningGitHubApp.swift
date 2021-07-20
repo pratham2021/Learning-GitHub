ff8f7dd7f5379b0c2767a992e11c83d4ced96393
Pratham Hebbar <prathamhebbar2021@gmail.com>
2021-07-19 18:50:19 -0700

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Pratham  Hebbar on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
